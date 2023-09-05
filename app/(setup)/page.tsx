5a3c7aab7f82d8458fc359e2193d6de72f8a7b16
Naeem Sani <muhdnaeem95@gmail.com>
2023-09-05 17:34:32 +0800

    first commit

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import InitialModal from "@/components/modals/initial-modal";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile"
+import { redirect } from 'next/navigation'
+
+const SetupPage = async () => {
+    const profile = await initialProfile();
+
+    // search all the servers and find the first one that has the profile
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (server) return redirect(`/servers/${server.id}`);
+
+    return (
+        <InitialModal />
+    )
+}
+
+export default SetupPage
---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+// prevents hot reload from generating too many prisma clients
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db
