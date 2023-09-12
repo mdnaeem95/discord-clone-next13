b84154f79b8b50fe4fb8089938b76b309e8c04c6
Naeem Sani <muhdnaeem95@gmail.com>
2023-09-12 17:56:09 +0800

    Added edit channel and delete channel functionality

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client"
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import ActionTooltip from "../action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
     channel: Channel;
@@ -19,6 +20,7 @@ const iconMap = {
 }
 
 const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
+    const { onOpen } = useModal();
     const router = useRouter();
     const params = useParams();
 
@@ -39,10 +41,16 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
             {channel.name !== "general" && role !== MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2">
                     <ActionTooltip label="Edit">
-                        <Edit className="w-4 h-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                        <Edit 
+                            className="w-4 h-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" 
+                            onClick={() => onOpen("editChannel", { server, channel })}
+                        />
                     </ActionTooltip>
-                    <ActionTooltip label="Edit">
-                        <Trash className="w-4 h-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                    <ActionTooltip label="Delete">
+                        <Trash 
+                            className="w-4 h-4 hidden group-hover:block text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" 
+                            onClick={() => onOpen("deleteChannel", { server, channel })}
+                        />
                     </ActionTooltip>
                 </div>
             )}
