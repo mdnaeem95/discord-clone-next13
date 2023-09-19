ab75a3aec6e1399e971e8dc896cc48f1a93f4ac9
Naeem Sani <muhdnaeem95@gmail.com>
2023-09-19 21:59:04 +0800

    Added chat messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -42,7 +43,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
                 type="channel" 
             />
 
-            <div className="flex-1">Messages</div>
+            <ChatMessages 
+                name={channel.name}
+                member={member}
+                chatId={channel.id}
+                type="channel"
+                apiUrl="/api/messages"
+                socketUrl="/api/socket/messages"
+                socketQuery={{
+                    channelId: channel.id,
+                    serverId: channel.serverId
+                }}
+                paramKey="channelId"
+                paramValue={channel.id}
+            />
 
             <ChatInput
                 name={channel.name}
