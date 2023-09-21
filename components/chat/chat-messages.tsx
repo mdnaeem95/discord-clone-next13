6c52f62edb1381da517e8b5d138ad77fa91cf890
Naeem Sani <muhdnaeem95@gmail.com>
2023-09-21 10:46:36 +0800

    Added chat socket updating in real time

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -30,6 +31,8 @@ type MessageWithMemberWithProfile = Message & {
 
 const ChatMessages = ({ name, member, chatId, apiUrl, socketUrl, socketQuery, paramKey, paramValue, type }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`
+    const updateKey = `chat:${chatId}:messages:update`
 
     const { data, fetchNextPage, isFetchingNextPage, hasNextPage, status } = useChatQuery({
         queryKey,
@@ -37,6 +40,8 @@ const ChatMessages = ({ name, member, chatId, apiUrl, socketUrl, socketQuery, pa
         paramKey,
         paramValue
     })
+
+    useChatSocket({ queryKey, addKey, updateKey});
 
     if (status === "loading") {
         return (
