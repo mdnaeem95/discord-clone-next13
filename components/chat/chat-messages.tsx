481aa743143c05e38f5908dfc2a6c0827fdf9559
Naeem Sani <muhdnaeem95@gmail.com>
2023-09-21 08:57:48 +0800

    added display of messages, added edit message functionality

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -5,6 +5,10 @@ import ChatWelcome from "./chat-welcome";
 import useChatQuery from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import ChatItem from "./chat-item";
+import { format } from "date-fns";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
 interface ChatMessagesProps {
     name: string;
@@ -64,9 +68,19 @@ const ChatMessages = ({ name, member, chatId, apiUrl, socketUrl, socketQuery, pa
                 {data?.pages?.map((group, i) => (
                     <Fragment key={i}>
                         {group.items.map((message: MessageWithMemberWithProfile) => (
-                            <div key={message.id}>
-                                {message.content}
-                            </div>
+                            <ChatItem
+                                currentMember={member}
+                                member={message.member}
+                                key={message.id}     
+                                id={message.id}
+                                content={message.content}
+                                fileUrl={message.fileUrl}
+                                deleted={message.deleted}
+                                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                                isUpdated={message.updatedAt !== message.createdAt}
+                                socketUrl={socketUrl}
+                                socketQuery={socketQuery}     
+                            />
                         ))}
                     </Fragment>
                 ))}
